65ef55871fb3cd637b42471caf06c79773557b85
YashasG98 <yashu191298@gmail.com>
2019-04-23 14:27:11 +0530

    added input form and form handling

--- to_do_app/lib/ui/ToDoScreen.dart
+++ to_do_app/lib/ui/ToDoScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/model/toDoItem.dart';
+import 'package:to_do_app/util/database_client.dart';
 
 class ToDoScreen extends StatefulWidget {
   @override
@@ -6,6 +8,19 @@ class ToDoScreen extends StatefulWidget {
 }
 
 class _ToDoScreenState extends State<ToDoScreen> {
+
+  final TextEditingController _textEditingController = new TextEditingController();
+  var db = new DatabaseHelper();
+
+  void _handleSubmitted(String text) async {
+    _textEditingController.clear();
+
+    ToDoItem toDoItem = new ToDoItem(text, DateTime.now().toIso8601String());
+    int savedItemId = await db.saveItem(toDoItem);
+    print("Item saved id: $savedItemId");
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,5 +38,32 @@ class _ToDoScreenState extends State<ToDoScreen> {
   }
 
   void _showFormDialog(){
+    var alert = AlertDialog(
+      content: Row(
+        children: <Widget>[
+          Expanded(
+            child:TextField(
+              controller: _textEditingController,
+              autofocus: true,
+              decoration: new InputDecoration(
+                labelText: "Item",
+                hintText: "eg. Buy stuff",
+                icon: Icon(Icons.note_add)
+              ),
+            )
+          )
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(onPressed: (){
+          _handleSubmitted(_textEditingController.text);
+          _textEditingController.clear();
+        }, child: Text("Save")),
+        new FlatButton(onPressed:() => Navigator.pop(context), child: Text("Cancel"))
+      ],
+    );
+    showDialog(context: context,builder:(_){
+      return alert;
+    });
   }
 }
