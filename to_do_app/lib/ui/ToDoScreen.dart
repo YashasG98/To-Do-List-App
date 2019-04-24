b37cd157fe309af667bf7b2afe6112effce6710d
YashasG98 <yashu191298@gmail.com>
2019-04-24 20:32:24 +0530

    added items display!

--- to_do_app/lib/ui/ToDoScreen.dart
+++ to_do_app/lib/ui/ToDoScreen.dart
@@ -8,62 +8,118 @@ class ToDoScreen extends StatefulWidget {
 }
 
 class _ToDoScreenState extends State<ToDoScreen> {
-
-  final TextEditingController _textEditingController = new TextEditingController();
+  final TextEditingController _textEditingController =
+      new TextEditingController();
   var db = new DatabaseHelper();
+  final List<ToDoItem> _itemList = <ToDoItem>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readToDoList();
+  }
 
   void _handleSubmitted(String text) async {
     _textEditingController.clear();
 
     ToDoItem toDoItem = new ToDoItem(text, DateTime.now().toIso8601String());
     int savedItemId = await db.saveItem(toDoItem);
+
+    ToDoItem addedItem = await db.getItem(savedItemId);
+
+    setState(() {
+      _itemList.insert(0, addedItem);
+    });
+
     print("Item saved id: $savedItemId");
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(),
-
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+                padding:EdgeInsets.all(8.0),
+                reverse:false,
+                itemCount: _itemList.length,
+                itemBuilder: (_, int index) {
+                  return Card(
+                    color: Colors.white10,
+                    child: ListTile(
+                      title: _itemList[index],
+                      onLongPress: () => debugPrint(""),
+                      trailing: Listener(
+                        key: Key(_itemList[index].itemName),
+                        child: Icon(
+                            Icons.remove_circle,
+                          color: Colors.red,
+                        ),
+                        onPointerDown: (pointerEvent) => debugPrint(""),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          new Divider(
+            height: 1.0,
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
           tooltip: "Add Item",
           backgroundColor: Colors.greenAccent,
           child: ListTile(
-            title: Icon(Icons.add,color: Colors.black,),
+            title: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
           ),
           onPressed: _showFormDialog),
     );
   }
 
-  void _showFormDialog(){
+  void _showFormDialog() {
     var alert = AlertDialog(
       content: Row(
         children: <Widget>[
           Expanded(
-            child:TextField(
-              controller: _textEditingController,
-              autofocus: true,
-              decoration: new InputDecoration(
+              child: TextField(
+            controller: _textEditingController,
+            autofocus: true,
+            decoration: new InputDecoration(
                 labelText: "Item",
                 hintText: "eg. Buy stuff",
-                icon: Icon(Icons.note_add)
-              ),
-            )
-          )
+                icon: Icon(Icons.note_add)),
+          ))
         ],
       ),
       actions: <Widget>[
-        new FlatButton(onPressed: (){
-          _handleSubmitted(_textEditingController.text);
-          _textEditingController.clear();
-        }, child: Text("Save")),
-        new FlatButton(onPressed:() => Navigator.pop(context), child: Text("Cancel"))
+        new FlatButton(
+            onPressed: () {
+              _handleSubmitted(_textEditingController.text);
+              _textEditingController.clear();
+            },
+            child: Text("Save")),
+        new FlatButton(
+            onPressed: () => Navigator.pop(context), child: Text("Cancel"))
       ],
     );
-    showDialog(context: context,builder:(_){
-      return alert;
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
+  }
+
+  _readToDoList() async {
+    List items = await db.getItems();
+    items.forEach((item) {
+      ToDoItem toDoItem = ToDoItem.map(item);
+      print("Db items: ${toDoItem.itemName}");
     });
   }
 }
